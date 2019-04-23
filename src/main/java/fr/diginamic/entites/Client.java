361389570e8f8faad34599c91950379eb317ba22
Cecile Peyras <peyrascecile@gmail.com>
2019-04-23 10:37:53 +0200

    fin TP 1.2

---
+++ src/main/java/fr/diginamic/entites/Client.java
@@ -0,0 +1,30 @@
+package fr.diginamic.entites;
+
+
+/**
+ * Classe qui représente le client
+ * @author Cécile Peyras
+ *
+ */
+public class Client {
+
+	public int numeroCompte;
+	public String nom;
+	public String prenom;
+
+	/**
+	 * Constructeur du client
+	 * @param param1 : numéro de compte
+	 * @param nomClient : nom du client
+	 * @param prenomClient : prénom du client
+	 */
+	public Client(int param1, String nomClient, String prenomClient) {
+		System.out.println("Nouvelle instance créée");
+		//System.out.println(param1);
+		numeroCompte = param1; 
+		nom = nomClient; 
+		prenom = prenomClient; 
+		System.out.println("Le numéro de compte du client " + nom + " " + prenom + " est : " + numeroCompte);
+	}
+
+}
---
+++ src/main/java/fr/diginamic/essais/TestClient.java
@@ -0,0 +1,21 @@
+package fr.diginamic.essais;
+
+import fr.diginamic.entites.Client;
+
+public class TestClient {
+
+	public static void main(String[] args) {
+		
+		Client client1 = new Client(25, "Peyras", "Cécile"); 
+		
+		Client client2 = new Client(27, "Dupont", "Jean"); 
+		
+		//System.out.println(client1.numeroCompte);
+		//System.out.println(client2.numeroCompte);
+		
+		
+		
+
+	}
+
+}
